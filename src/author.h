c4730079c81c8d321ae8d4c2be7e1775aaba1242
kyonides <kyonides@gmail.com>
2024-05-19 01:32:28 -0600

    Bug Fix: SoundFont Constants Now Have Default Values

--- src/author.h
+++ src/author.h
@@ -1,6 +1,6 @@
 #define HIDDENAUTHOR "Kyonides Arkanthes"
-#define HIDDENVERSION "1.1.63"
-#define HIDDENDATE "2024-05-18"
+#define HIDDENVERSION "1.1.64"
+#define HIDDENDATE "2024-05-19"
 #define CODENAME "ReConfigured"
 #define SYSTEM_STRING "linux"
 #define SYSTEM_REAL_STRING "Linux"
